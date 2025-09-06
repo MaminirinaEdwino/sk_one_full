08883c5738d8076bcd9d7301b56b361f7e7a70cd
MaminirinaEdwino <edwinomaminirina@gmail.com>
2025-09-06 22:01:47 +0300

    modif fixtures

--- src/DataFixtures/EmployeeFixtures.php
+++ src/DataFixtures/EmployeeFixtures.php
@@ -3,8 +3,10 @@
 namespace App\DataFixtures;
 
 use App\Entity\Employee;
+use App\Entity\Poste;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class EmployeeFixtures extends Fixture
 {
@@ -12,9 +14,27 @@ class EmployeeFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
-        $emp = new Employee();
+        $faker = Factory::create('fr_FR');
+        for ($i=0; $i < 10; $i++) { 
+            $emp = new Employee();
+            $emp->setNom($faker->name);
+            $emp->setPrenom($faker->firstName);
+            $emp->setEmailpro($faker->email);
+            $emp->setTelephone($faker->phoneNumber);
+            $emp->setAddresse($faker->address());
+            $emp->addPoste($this->getReference('poste_'.mt_rand(0,4), Poste::class));
+            $emp->setDateEntree($faker->dateTime);
+            $emp->setDateSortie($faker->datetime);
+        }
         
 
         $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            PosteFixtures::class,
+        ];
     }
 }
--- src/DataFixtures/PosteFixtures.php
+++ src/DataFixtures/PosteFixtures.php
@@ -12,11 +12,13 @@ class PosteFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
-        for ($i=0; $i < 2; $i++) { 
+        for ($i=0; $i < 5; $i++) { 
             $poste = new Poste();
             $poste->setNom($faker->word);
             $poste->setDescription($faker->sentence);
             $manager->persist($poste);
+
+            $this->addReference('poste_'.$i, $poste);
         }
 
         $manager->flush();
